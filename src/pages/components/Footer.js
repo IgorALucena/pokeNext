81bf00f89159c87eafc5b21182351e6a98e96a39
Igor <igoralbuquerque021@gmail.com>
2024-05-05 09:19:32 -0300

    chore: Adding CSS to the header and footer

--- src/pages/components/Footer.js
+++ src/pages/components/Footer.js
@@ -1,8 +1,10 @@
+import styles from '../../styles/Footer.module.css'
+
 export default function Footer(){
     return(
-        <footer>
+        <footer className={styles.footer}>
             <p>
-                <span>PokeNext</span> &copy; 2021
+                <span>PokeNext</span> &copy; 2024
             </p>
         </footer>
     )
