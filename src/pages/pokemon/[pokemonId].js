aa790b5b391af5b02793986df53f15f5930bf8ed
Igor <igoralbuquerque021@gmail.com>
2024-05-05 12:37:51 -0300

    style: Styling the Individual Page

--- src/pages/pokemon/[pokemonId].js
+++ src/pages/pokemon/[pokemonId].js
@@ -1,3 +1,6 @@
+import Image from 'next/image'
+import styles from '../../styles/Pokemon.module.css'
+
 export const getStaticProps = async (context) => {
 
     const id = context.params.pokemonId;
@@ -6,8 +9,8 @@ export const getStaticProps = async (context) => {
 
     const data = await res.json();
 
-    return{
-        props:{pokemon:data},
+    return {
+        props: { pokemon: data },
     }
 
 }
@@ -21,22 +24,61 @@ export const getStaticPaths = async () => {
 
     const data = await res.json();
 
-    //Params: api não tem id
+    //Params: api não tem id. Criado manualmente.
 
-    const paths = data.results.map((pokemon, index) =>{
+    const paths = data.results.map((pokemon, index) => {
         return {
-            params: {pokemonId: (index+1).toString()},
+            params: {
+                pokemonId: (index + 1).toString()
+            },
         }
     })
 
-    return{
+    return {
         paths,
-        fallback:false,
+        fallback: false,
     }
 
 }
 
-
 export default function Pokemon({ pokemon }) {
-    return <p>{pokemon.name}</p>
+
+    return (
+        <div className={styles.pokemon_container}>
+            <h1 className={styles.title}>{pokemon.name}</h1>
+            <Image
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`}
+                width="200"
+                height="200"
+                alt={pokemon.name}
+            />
+            <div>
+                <h3>Número:</h3>
+                <p>#{pokemon.id}</p>
+            </div>
+            <div>
+                <h3>Tipo:</h3>
+                <div className={styles.types_container}>
+                    {pokemon.types.map((item, index) => (
+                        <span
+                            key={index}
+                            className={`${styles.type} ${styles['type_' + item.type.name]}`} // classe dinamica para css
+                        >
+                            {item.type.name}
+                        </span>
+                    ))}
+                </div>
+            </div>
+            <div className={styles.data_container}>
+                <div className={styles.data_height}>
+                    <h4>Altura:</h4>
+                    <p>{pokemon.height * 10} cm</p>
+                </div>
+                <div className={styles.data_weight}>
+                    <h4>Peso:</h4>
+                    <p>{pokemon.weight / 10} kg</p>
+                </div>
+            </div>
+        </div>
+    )
 }
