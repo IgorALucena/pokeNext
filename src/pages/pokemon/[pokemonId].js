e1905de9161777c1da58a6273ee7159099bd5b69
Igor <igoralbuquerque021@gmail.com>
2024-05-05 11:32:16 -0300

    feat: Creating Individual Dynamic Page for Each Pokémon

---
+++ src/pages/pokemon/[pokemonId].js
@@ -0,0 +1,42 @@
+export const getStaticProps = async (context) => {
+
+    const id = context.params.pokemonId;
+
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+
+    const data = await res.json();
+
+    return{
+        props:{pokemon:data},
+    }
+
+}
+
+export const getStaticPaths = async () => {
+
+    const maxPokemons = 251;
+    const api = `https://pokeapi.co/api/v2/pokemon/`;
+
+    const res = await fetch(`${api}/?limit=${maxPokemons}`);
+
+    const data = await res.json();
+
+    //Params: api não tem id
+
+    const paths = data.results.map((pokemon, index) =>{
+        return {
+            params: {pokemonId: (index+1).toString()},
+        }
+    })
+
+    return{
+        paths,
+        fallback:false,
+    }
+
+}
+
+
+export default function Pokemon({ pokemon }) {
+    return <p>{pokemon.name}</p>
+}
